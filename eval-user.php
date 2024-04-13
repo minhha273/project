51687eeedd566a680741610f60a74fa0d2d4ae4c
tsof-smoky <107832241+tsof-smoky@users.noreply.github.com>
2024-04-13 19:43:27 +0700

    eval-user_vuln.php

--- eval-user.php
+++ eval-user.php
@@ -2,18 +2,7 @@
 
 $user_input = $_GET['input'];
 
-$sanitized_input = preg_replace('/[^0-9+]/', '', $user_input);
-
-$result = my_eval($sanitized_input);
-
-function my_eval($input) {
-    if (preg_match('/^\d+\+\d+$/', $input)) {
-        eval("\$result = $input;");
-        return $result;
-    } else {
-        return "Invalid input";
-    }
-}
+$result = eval($user_input);
 
 echo "Result: $result";
 ?>
